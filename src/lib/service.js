23534f243a21440c080341d1499b33f4ee07303a
cemcelikgh <cemcelik@engineer.com>
2025-02-16 15:43:13 +0300

    nothing

--- src/lib/service.js
+++ src/lib/service.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 async function getData(userID) {
   try {
     const isEmty = userID;
-    if(typeof userID === 'undefined' || String(isEmty).trim() == '') {throw 'Please enter a User ID'};
-    if(isNaN(userID)) {throw 'Please enter a User ID that is a number'};
-    if(userID < 1) {throw 'Please enter a User ID that is high from 0'};
+    if(typeof userID === 'undefined' || String(isEmty).trim() == '') throw 'Please enter a User ID';
+    if(isNaN(userID)) throw 'Please enter a User ID that is a number';
+    if(userID < 1) throw 'Please enter a User ID that is high from 0';
     const userResponse = await axios.get(`https://jsonplaceholder.typicode.com/users/${userID}`);
     const userInfo = userResponse.data;
     const postsResponse = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${userID}`);
@@ -22,9 +22,7 @@ async function getData(userID) {
       posts: userPosts
     }
     return userData;
-  } catch (error) {
-    console.error(error);
-  }
+  } catch (error) {console.error(error)};
 }
 
 export default getData;
